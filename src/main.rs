6b6a5d611fe420f68661b06d1b686259e4cbec06
Berzan <berzan@tutanota.com>
2023-10-14 16:34:51 +0000

    play: with `Client`s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+    net::{Ipv4Addr, SocketAddrV4},
+    time::Duration,
+};
+
+use client::Client;
+
+mod client;
+
 fn main() {
-    println!("Hello, world!");
+    let first_client = Client::new(3555).unwrap();
+    let second_client = Client::new(3556).unwrap();
+
+    std::thread::spawn(move || loop {
+        std::thread::sleep(Duration::from_secs(1));
+        second_client.send(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3555), "hello world")
+    });
+
+    first_client.listen()
 }
