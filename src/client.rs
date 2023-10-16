479515537c65b89c192106824744598dec150c2d
Berzan <berzan@tutanota.com>
2023-10-16 14:29:01 +0000

    rewrite: `client.rs` file

--- src/client.rs
+++ src/client.rs
@@ -1,44 +1,109 @@
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
+use std::{
+    io::BufRead,
+    net::{SocketAddr, UdpSocket},
+    thread::{sleep, spawn},
+    time::Duration,
+};
 
-/// Client for the blockchain.
+use crate::message::Message;
+
 pub struct Client {
     socket: UdpSocket,
+    others: Vec<SocketAddr>,
 }
 
 impl Client {
-    /// Creates a new client on localhost with given port.
-    /// # Usage
-    /// ```rs
-    /// let client = Client::new(3555).unwrap();
-    /// ```
-    pub fn new(port: u16) -> Option<Client> {
-        let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
-        let socket = UdpSocket::bind(addr).ok()?;
-        Some(Client { socket })
+    pub fn new() -> Result<Client, ()> {
+        let socket = UdpSocket::bind("127.0.0.1:5555").map_err(|_| ())?;
+        Ok(Client {
+            socket,
+            others: vec![],
+        })
     }
 
-    /// Listens for incoming messages and prints them.
-    pub fn listen(&self) {
-        let mut buf = [0; 1000];
-        loop {
-            match self.socket.recv_from(&mut buf) {
-                Ok((number_of_bytes, sender)) => {
-                    let msg_buf = &buf[..number_of_bytes];
-                    let msg = String::from_utf8_lossy(msg_buf);
+    #[cfg(test)]
+    pub fn new_test() -> Result<Client, ()> {
+        let socket = UdpSocket::bind("127.0.0.1:4444").map_err(|_| ())?;
+        Ok(Client {
+            socket,
+            others: vec![],
+        })
+    }
 
-                    println!("sender: {sender}\nmessage:{msg}\n");
+    pub fn join_consensus(&mut self, client_addr: SocketAddr) -> Result<(), ()> {
+        let msg = Message::JoinConsensus {};
+
+        self.send_msg(msg, client_addr)?;
+
+        let mut buf = [0u8; 1000];
+
+        let (msg, from) = self.recv_msg(&mut buf).map_err(|_| ())?;
+
+        if from != client_addr {
+            return Err(());
+        }
+
+        match msg {
+            Message::OtherClients { addrs } => {
+                self.others = addrs;
+                Ok(())
+            }
+            _ => Err(()),
+        }
+    }
+
+    fn recv_msg(&self, buf: &mut [u8]) -> Result<(Message, SocketAddr), ()> {
+        match self.socket.recv_from(buf) {
+            Ok((len, from)) => Ok((buf[0..len].into(), from)),
+            Err(_) => Err(()),
+        }
+    }
+
+    fn send_msg(&self, msg: Message, to: SocketAddr) -> Result<(), ()> {
+        let buf: Vec<u8> = msg.into();
+        self.socket.send_to(&buf, to).map(|_| ()).map_err(|_| ())
+    }
+
+    pub fn start(&mut self) {
+        let mut buf = [0u8; 1000];
+
+        while let Ok((msg, from)) = self.recv_msg(&mut buf) {
+            match msg {
+                Message::JoinConsensus {} => {
+                    self.others.push(from);
+
+                    let msg = Message::OtherClients {
+                        addrs: self.others.clone(),
+                    };
+
+                    if self.send_msg(msg, from).is_err() {
+                        eprintln!("couldn't send message to {:#?}", from)
+                    }
                 }
-                Err(err) => eprintln!("an error occured during receiving message: {}", err.kind()),
+                Message::OtherClients { addrs: _ } => (),
+                Message::NewClient { addr } => {
+                    self.others.push(addr);
+                }
+                Message::Error {} => {
+                    eprintln!("mistaken message sent by {:#?}", from);
+                }
             }
         }
     }
+}
 
-    /// Sends given message to given receiver.
-    pub fn send(&self, receiver: SocketAddrV4, msg: &str) {
-        let buf = msg.as_bytes();
-        match self.socket.send_to(buf, receiver) {
-            Ok(_) => (),
-            Err(err) => eprintln!("an error occured during sending message: {}", err.kind()),
-        }
-    }
+#[test]
+fn test_client() {
+    let mut client = Client::new().unwrap();
+    let mut client2 = Client::new_test().unwrap();
+
+    spawn(move || client.start());
+    spawn(move || {
+        client2
+            .join_consensus("127.0.0.1:5555".parse().unwrap())
+            .unwrap();
+        println!("joined consensus");
+    });
+
+    sleep(Duration::from_secs(5));
 }
