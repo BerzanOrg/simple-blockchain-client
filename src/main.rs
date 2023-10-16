597a42f821b6bda303eb8c89ef1e7ba2319c847e
Berzan <berzan@tutanota.com>
2023-10-16 14:28:51 +0000

    add: new module

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
-use std::{
-    net::{Ipv4Addr, SocketAddrV4},
-    time::Duration,
-};
-
-use client::Client;
-
 mod client;
+mod message;
 
-fn main() {
-    let first_client = Client::new(3555).unwrap();
-    let second_client = Client::new(3556).unwrap();
-
-    std::thread::spawn(move || loop {
-        std::thread::sleep(Duration::from_secs(1));
-        second_client.send(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3555), "hello world")
-    });
-
-    first_client.listen()
-}
+fn main() {}
